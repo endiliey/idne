31532a688e5ed93e2be3bfbff9269c28791c1d3c
Endiliey <endiliey@gmail.com>
2017-05-11 00:24:57 +0800

    Add more macros

--- template.cpp
+++ template.cpp
@@ -6,17 +6,32 @@ typedef vector<int> vi;
 typedef vector<vi> vvi; 
 typedef pair<int,int> ii; 
 #define sz(a) int((a).size()) 
-#define pb push_back 
+#define PB push_back 
+#define MP make_pair
+#define F first
+#define S second
 #define all(c) (c).begin(),(c).end() 
+#define REP(i,a,b) for (int i = a; i < b; ++i)
+#define REP2(i,a,b) for (int i = a; i <= b; ++i)
 #define tr(c,i) for(decltype((c).begin()) i = (c).begin(); i != (c).end(); i++) 
 #define present(c,x) ((c).find(x) != (c).end()) 
 #define cpresent(c,x) (find(all(c),x) != (c).end())
+#define fill(a, v) memset(a, v, sizeof a)
 typedef long long ll;
+
+ll modpow(ll x, ll n, ll m) 
+{
+    if (n == 0) return 1 % m;
+    ll u = modpow(x, n/2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1) u = (u * x) % m;
+    return u;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    // start code
+    // start code here
     return 0;
 }
