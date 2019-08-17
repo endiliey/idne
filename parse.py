e4e466a3852caf58c3254cc8f360c7d72fe38cdf
Chaigidel <32701369+Chaigidel@users.noreply.github.com>
2019-08-17 22:53:15 +0800

    Add 'A1 A2'-style problem support (#10)

--- parse.py
+++ parse.py
@@ -105,7 +105,7 @@ class CodeforcesContestParser(HTMLParser):
                 self.start_contest = True
         elif tag == 'option':
             if len(attrs) == 1:
-                regexp = re.compile(r"'[A-Z]'") # The attrs will be something like: ('value', 'X')
+                regexp = re.compile(r"'[A-Z]([0-9]|\b)'") # The attrs will be something like: ('value', 'X')
                 string = str(attrs[0])
                 search = regexp.search(string)
                 if search is not None:
